6aa03d4a5090fae91005f4c24d45b643e1cad76a
Fenneig <ninetailedkot92@gmail.com>
2023-09-13 02:19:30 +0300

    add input handler

---
+++ Assets/Scripts/Systems/InputHandler.cs
@@ -0,0 +1,28 @@
+﻿using SplatTheRat.Components.Interfaces;
+using UnityEngine;
+
+namespace SplatTheRat.Systems
+{
+    public class InputHandler : MonoBehaviour
+    {
+        private Camera _mainCamera;
+        
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (!Input.GetMouseButtonDown(0)) return;
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (!Physics.Raycast(ray, out var hit)) return;
+            
+            if (hit.collider.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.Damage();
+            }
+        }
+    }
+}
